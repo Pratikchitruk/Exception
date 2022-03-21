9a9e6f0c481c50b6093ffd00f854b1193a056a84
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-21 21:55:03 +0530

    Add files via upload

---
+++ MobileNo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class MobileNo
+    {
+        float mobileno = 8208508814;
+
+        public void EnterNumber(float Number)
+        {
+            if (mobileno == 8208508814)
+            {
+                throw (new FirstnameException("number found"));
+
+
+            }
+            else
+            {
+                Console.WriteLine("the email is " + Number);
+            }
+
+        }
+    }
+}
